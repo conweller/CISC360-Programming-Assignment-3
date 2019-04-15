94ab1e50eed3698042ee74026d3c3eadde6b7d0b
Connor Onweller <onweller@udel.edu>
2019-04-14 23:26:30 -0400

    added signatures to redirect

--- redirect.c
+++ redirect.c
@@ -7,6 +7,20 @@
  * one command to a file                                *
  ********************************************************/
 
+/*
+ *  Function: redirect
+ *  ------------------
+ *    Attempts to write the output of one command to a file.
+ *
+ *    cmd_argv: the arguments for the command
+ *    cmd_argc: the number of arguments for the command
+ *    filename: the file to be written to
+ *    options: options given to the function (bitwise orred together):
+ *        REDIR_OW: overwrite existing file, if the inputted filename exists
+ *        REDIR_ER: write std_err to file as well as std_out
+ *        REDIR_AP: append to the file
+ */
+
 void redirect(char ** cmd_argv, int cmd_argc, char* filename, int options) {
 
 }
--- redirect.h
+++ redirect.h
@@ -7,10 +7,25 @@
  * one command to a file                                *
  ********************************************************/
 
+#include <stdlib.h>
+#include <string.h>
+
+const int REDIR_OW = 1;
+const int REDIR_ER = 2;
+const int REDIR_AP = 4;
+
 /*
  *  Function: redirect
  *  ------------------
+ *    Attempts to write the output of one command to a file.
  *
- *
+ *    cmd_argv: the arguments for the command
+ *    cmd_argc: the number of arguments for the command
+ *    filename: the file to be written to
+ *    options: options given to the function (bitwise orred together):
+ *        REDIR_OW: overwrite existing file, if the inputted filename exists
+ *        REDIR_ER: write std_err to file as well as std_out
+ *        REDIR_AP: append to the file
  */
+
 void redirect(char ** cmd_argv, int cmd_argc, char* filename, int options);
